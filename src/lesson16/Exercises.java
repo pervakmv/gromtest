233af92114ac799d34233bc913a9aa8e77f8a7e8
Nikolay <pervakmv@elas.kiev.ua>
2018-05-24 15:25:47 +0300

    Воюю з 17-м уроком. правка

--- src/lesson16/Exercises.java
+++ src/lesson16/Exercises.java
@@ -27,7 +27,7 @@ public class Exercises {
         for (int i = 0; i < words.length; i++) {
             for (int j = i + 1; j < words.length; j++) {
                 if (words[i].equals(words[j]))
-                    words[i] = "";
+                    words[j] = "";
 
             }
         }
