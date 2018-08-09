e9572658e495c39a26948826bdca12966fcee0d7
Nikolay <pervakmv@elas.kiev.ua>
2018-08-09 17:37:11 +0300

    Урок 32

---
+++ src/lesson33/HW2/Demo.java
@@ -0,0 +1,8 @@
+package lesson33.HW2;
+
+public class Demo {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.readFileByConsolePath();
+    }
+}
